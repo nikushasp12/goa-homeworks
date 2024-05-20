0a680efc39642bbe57d203e9e785ec093be6cfd7
nikushasp12 <nikagadela15002gmail.com>
2024-05-20 19:42:23 +0400

    third commit

---
+++ level 3/level 3.py
@@ -0,0 +1,35 @@
+print('homework')
+#სახელი,გვარი,ასაკი,ჰობი,ქვეყანა.
+name ="nikolozi"
+print(name)
+
+last_name = 'gadelia'
+print(last_name)
+
+age ='14'
+print(age)
+
+hobby='basketball'
+print(hobby)
+COUNTRY= 'GEORGIA'
+print(COUNTRY)
+#ცვლადების ერთმანეთზე დამატება
+print(last_name+age)
+print(COUNTRY+hobby)
+#10 მაგალითი მათემატიკურ ოპერატორებზე
+# მიმატება
+print(50 +50)
+print(60 +110)
+# გამოკლება
+print(100-90)
+print(90-29)
+#გაყოფა
+print(1000/200)
+print(20/5)
+#გაყოფა დამრგვალებით
+print(100//30)
+print(40//15)
+#გამრავლება
+print(10*6)
+#ახარისხება
+print(10**20)
