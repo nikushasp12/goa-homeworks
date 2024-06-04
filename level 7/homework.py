80554e05156ee0aee80dc896bfd111a2f2479fd8
nikushasp12 <nikagadela15002gmail.com>
2024-06-04 16:56:15 +0400

    the eighth commit

---
+++ level 7/homework.py
@@ -0,0 +1,56 @@
+
+name ="nika"
+last_name= 'gadelia'
+age=14
+country="GEORGIA"
+favorite_car='nissan_skyline'
+favorite_color='blue'
+favorite_number=14
+
+
+
+
+
+
+
+input("what is your name:   ")
+input("enter your name:  ")
+
+input("what is your last name:  ")
+input("enter your last_name:  ")
+
+input("what is your age:  ")
+input("enter your age:  ")
+
+input("which country do you live:  ")
+input("enter your country:  ")
+
+input("what is your favorite_car:")
+
+input("what is your favorite_color:")
+
+
+input("what is your favorite_number:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
